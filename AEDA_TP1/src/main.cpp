fbb1504ac8b6a9f55b322172d959bb10181d910a
JDSBCC <joo_correia@hotmail.com>
2013-10-01 13:00:52 +0100

    Alteraçoes: - o menu ler ja funciona para alunos - nas restantes entidades so verifica se o array esta vazio

--- AEDA_TP1/src/main.cpp
+++ AEDA_TP1/src/main.cpp
@@ -221,7 +221,27 @@ bool criar(int i) {
 }
 
 bool ler(int i) {
-
+	if(i==1){
+		if(alunos.empty()){
+			cout<<"Nao existem alunos para ler"<<endl;
+		}else{
+			for(unsigned int i=0;i<alunos.size();i++){
+				cout<<"Nome: "<<alunos[i].getNome()<<" - Numero: "<<alunos[i].getNumero()<<" - Turma: "<<alunos[i].getIDturma()<<endl;
+			}
+		}
+	}else if(i==2){
+		if(professores.empty()){
+			cout<<"Nao existem professores para ler"<<endl;
+		}
+	}else if(i==3){
+		if(turmas.empty()){
+			cout<<"Nao existem turmas para ler"<<endl;
+		}
+	}else{
+		if(disciplinas.empty()){
+			cout<<"Nao existem disciplinas para ler"<<endl;
+		}
+	}
 }
 
 bool actualizar(int i) {
