38d6f61f0b5ab2eaff5dbe02d12853bc63167101
fabiommc <fabiommc@outlook.com>
2013-11-11 23:25:16 +0000

    Doxygen Updated

--- AEDA_TP1/html/search/functions_73.js
+++ AEDA_TP1/html/search/functions_73.js
@@ -1,9 +1,16 @@
 var searchData=
 [
   ['setaluno',['setAluno',['../class_escola.html#a56f72e713a433a9b63086df24de6985a',1,'Escola']]],
+  ['setanoescolar',['setAnoEscolar',['../class_turma.html#abd1be35d1d394aeca1818ed643845ff8',1,'Turma']]],
   ['setdisciplina',['setDisciplina',['../class_escola.html#ac117e7e2d2f9c2ef22b41b6dc7a96708',1,'Escola']]],
+  ['setduracao',['setDuracao',['../class_disciplina.html#aadc65f8c2f09352cc6f80881d67e720d',1,'Disciplina']]],
+  ['sethorainicio',['setHoraInicio',['../class_disciplina.html#aa6822138c1277be2b2c5c420c1a1f34d',1,'Disciplina']]],
+  ['sethorario',['setHorario',['../class_turma.html#aab28fe642d927ba176ec4259fdc2d8e0',1,'Turma']]],
+  ['setid',['setID',['../class_turma.html#a5c011a25251cd40d549e504b71d7b18f',1,'Turma']]],
+  ['setnome',['setNome',['../class_disciplina.html#a2d8e0957375f6d9f1f720a58acfb80de',1,'Disciplina']]],
+  ['setnumero',['setNumero',['../class_aluno.html#a36ad83483a9e0c247d7092a95607758c',1,'Aluno']]],
   ['setprofessor',['setProfessor',['../class_escola.html#a69bc6d8eb743a508d2b805285fd59e35',1,'Escola']]],
-  ['setturma',['setTurma',['../class_escola.html#a75635f871c420f3d1373285b1e773ce8',1,'Escola']]],
+  ['setturma',['setTurma',['../class_aluno.html#a5403cd3d2d1a219f9aa49bc53825d4fc',1,'Aluno::setTurma()'],['../class_escola.html#a75635f871c420f3d1373285b1e773ce8',1,'Escola::setTurma()']]],
   ['showaluno',['showAluno',['../class_escola.html#a61476572624adfbc5ff2a0dce1e8ce0b',1,'Escola']]],
   ['showdisciplina',['showDisciplina',['../class_escola.html#a3578c85887dd28c9d0b6f7c7f10f3020',1,'Escola']]],
   ['showprofessor',['showProfessor',['../class_escola.html#addc6cd7b85c6fdba76dd536c580c16c8',1,'Escola']]],
